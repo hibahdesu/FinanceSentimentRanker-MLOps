051bd1e10aea113f1c2805d175894be1cd65c532
hibahdesu <hibahsindi@gmail.com>
2025-03-14 22:27:29 +0300

    Updating

--- src/pipeline/split_data.py
+++ src/pipeline/split_data.py
@@ -1,7 +1,8 @@
 from sklearn.model_selection import train_test_split
 
+s = 101
 def split_data(df):
     X = df['news']
     y = df['compound']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=s)
     return X_train, X_test, y_train, y_test
